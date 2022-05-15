9ed9bfd32ac835bfd9c9f8f671bda0aaf912feb9
ManOfIntrigue <52455008+ManOfIntrigue@users.noreply.github.com>
2022-05-15 15:40:32 -0500

    Added Mapping Updates

--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -54,6 +54,7 @@ public class TestInput : MonoBehaviour
             Debug.Log("Joystick Stick (x,y): " + joystickStick.ToString());
         }
 
+        /*
         if (joystick.hatswitch != null)
         {
             var joystickHatSwitch = joystick.hatswitch.ReadValue();
@@ -62,7 +63,7 @@ public class TestInput : MonoBehaviour
                 Debug.Log("Joystick Hat Switch (x,y): " + joystickStick.ToString());
             }
         }
-        
+        */
 
         /*
         var joystickControls_X = joystick.allControls[1];
@@ -91,7 +92,7 @@ public class TestInput : MonoBehaviour
             Debug.Log("joystickControls_L1 Pressed");
         }
 
-        // 03: 
+        // 03: R3
         var joystickControls_R3 = joystick.allControls[3];
         if (joystickControls_R3.IsPressed())
         {
@@ -99,67 +100,124 @@ public class TestInput : MonoBehaviour
         }
 
 
-        // 04: 
+        // 04: L3
         var joystickControls_L3 = joystick.allControls[4];
         if (joystickControls_L3.IsPressed())
         {
             Debug.Log("joystickControls_L3 Pressed");
         }
 
-        // 05: 
+        // 05: Square
         var joystickControls_Square = joystick.allControls[5];
         if (joystickControls_Square.IsPressed())
         {
             Debug.Log("joystickControls_Square Pressed");
         }
 
-        // 06: 
+        // 06: X
         var joystickControls_X = joystick.allControls[6];
         if (joystickControls_X.IsPressed())
         {
             Debug.Log("joystickControls_X Pressed");
         }
 
-        // 07: 
+        // 07: O
         var joystickControls_O = joystick.allControls[7];
         if (joystickControls_O.IsPressed())
         {
             Debug.Log("joystickControls_O Pressed");
         }
 
-        // 08: 
+        // 08: Triangle
         var joystickControls_Triangle = joystick.allControls[8];
         if (joystickControls_Triangle.IsPressed())
         {
             Debug.Log("joystickControls_Triangle Pressed");
         }
 
-        // 09: 
+        // 09: R2
         var joystickControls_R2 = joystick.allControls[9];
         if (joystickControls_R2.IsPressed())
         {
             Debug.Log("joystickControls_R2 Pressed");
         }
 
-        // 10: 
+        // 10: L2
         var joystickControls_L2 = joystick.allControls[10];
         if (joystickControls_L2.IsPressed())
         {
             Debug.Log("joystickControls_L2 Pressed");
         }
 
-        // 11: 
+        // 11: Share
         var joystickControls_Share = joystick.allControls[11];
         if (joystickControls_Share.IsPressed())
         {
             Debug.Log("joystickControls_Share Pressed");
         }
 
-        // 12: 
+        // 12: Options
         var joystickControls_Options = joystick.allControls[12];
         if (joystickControls_Options.IsPressed())
         {
             Debug.Log("joystickControls_Options Pressed");
+        }
+
+
+        // 13: Thruster Tilt
+        var joystickControls_Thruster_Tilt = joystick.allControls[13];
+        if (joystickControls_Thruster_Tilt.IsPressed())
+        {   
+            Debug.Log("Thruster Tilt: " + joystickControls_Thruster_Tilt.ReadValueAsObject().ToString());
+        }
+
+        /*
+        // 14: 
+        var joystickControls_14 = joystick.allControls[14];
+        if (joystickControls_14.IsPressed())
+        {
+            // Debug.Log("joystickControls_14 Pressed");
+            Debug.Log("joystickControls_14: " + joystickControls_14.ReadValueAsObject().ToString());
+        }
+
+        // 15: 
+        var joystickControls_15 = joystick.allControls[15];
+        if (joystickControls_15.IsPressed())
+        {
+            // Debug.Log("joystickControls_15 Pressed");
+            Debug.Log("joystickControls_15: " + joystickControls_15.ReadValueAsObject().ToString());
+        }
+
+        // 16: 
+        var joystickControls_16 = joystick.allControls[16];
+        if (joystickControls_16.IsPressed())
+        {
+            // Debug.Log("joystickControls_16 Pressed");
+            Debug.Log("joystickControls_16: " + joystickControls_16.ReadValueAsObject().ToString());
+        }
+
+        // 17: 
+        var joystickControls_17 = joystick.allControls[17];
+        if (joystickControls_17.IsPressed())
+        {
+            // Debug.Log("joystickControls_17 Pressed");
+            Debug.Log("joystickControls_17: " + joystickControls_17.ReadValueAsObject().ToString());
+        }
+        */
+
+        // 18: Thruster
+        var joystickControls_Thruster = joystick.allControls[18];
+        if (joystickControls_Thruster.IsPressed())
+        {            
+            Debug.Log("Thruster: " + joystickControls_Thruster.ReadValueAsObject().ToString());
+        }
+
+        // 19: Hat Switch 
+        var joystickControls_Hat_Switch = joystick.allControls[19];
+        if (joystickControls_Hat_Switch.IsPressed())
+        {
+            // Debug.Log("joystickControls_19 Pressed");
+            Debug.Log("joystickControls_Hat_Switch (x,y): " + joystickControls_Hat_Switch.ReadValueAsObject().ToString());
         }
 
 
