731c62bef19a7a38e5f77f21d1c190843ddf4593
ManOfIntrigue <52455008+ManOfIntrigue@users.noreply.github.com>
2022-11-27 16:28:20 -0600

    Updated HUD, Added Physics to Ship

--- Assets/Scripts/TestShipInputEvents.cs
+++ Assets/Scripts/TestShipInputEvents.cs
@@ -6,9 +6,7 @@ using Assets.Scripts;
 // [ExecuteInEditMode] // Be carefull with this one
 [RequireComponent(typeof(MeshRenderer))]
 public class TestShipInputEvents : MonoBehaviour
-{
-    public float rotationSpeed;
-    public float movementSpeed;
+{    
     private float neutralSpeed = 0.0f;
     private float negativeRotationSpeed;
     private Vector3 nextRotationTransformation;
@@ -17,7 +15,26 @@ public class TestShipInputEvents : MonoBehaviour
     private float nextHorizontalMovement = 0.0f;
     private float movementThreshold = 0.01f;
     private float movementThresholdDiagonal = 0.1f;
+
     public TextMeshProUGUI _textMesh;
+
+
+    public float rotationSpeed;        
+    public float accelerationRate;    
+    public float resistance;
+
+    public float topDepthSpeed;
+    private float depthSpeed;
+    private float currentDepthControlValue = 0.0f;
+
+    public float topVerticalSpeed;
+    private float verticalSpeed;
+    private float currentVerticalControlValue = 0.0f;
+
+    public float topHorizontalSpeed;
+    private float horizontalSpeed;
+    private float currentHorizontalControlValue = 0.0f;
+    
 
     private Quaternion originalRotation;
     
@@ -40,16 +57,29 @@ public class TestShipInputEvents : MonoBehaviour
             transform.Rotate(nextRotationTransformation);
         }
 
+        UpdateMovementSpeed(currentDepthControlValue, ref depthSpeed, ref topDepthSpeed);
+        UpdateMovementSpeed(currentVerticalControlValue, ref verticalSpeed, ref topVerticalSpeed);
+        UpdateMovementSpeed(currentHorizontalControlValue, ref horizontalSpeed, ref topHorizontalSpeed);
+
         // Forward / Backward movement.
-        transform.position += transform.right * movementSpeed * nextMovement * Time.deltaTime;
+        // transform.position += transform.right * movementSpeed * nextMovement * Time.deltaTime;
+        transform.position += transform.right * depthSpeed * Time.deltaTime;
 
         // Horizontal movement.
-        transform.position += transform.up * movementSpeed * nextHorizontalMovement * Time.deltaTime;
+        // transform.position += transform.up * movementSpeed * nextHorizontalMovement * Time.deltaTime;
+        transform.position += transform.up * horizontalSpeed * Time.deltaTime;
 
         // Vertical movement.
-        transform.position += transform.forward * movementSpeed * nextVerticalMovement * Time.deltaTime;
+        // transform.position += transform.forward * movementSpeed * nextVerticalMovement * Time.deltaTime;
+        transform.position += transform.forward * verticalSpeed * Time.deltaTime;
 
-        _textMesh.text = "Rotation: " + transform.rotation;
+        // Update HUD
+        _textMesh.text = "Rotation: " + transform.rotation + "\n" 
+            + "nextMovement: " + nextMovement + "\n"
+            + "currentThrusterValue: " + currentDepthControlValue + "\n"
+            + "depthSpeed: " + depthSpeed + "\n"
+            + "verticalSpeed: " + verticalSpeed + "\n"
+            + "horizontalSpeed: " + horizontalSpeed;
     }
         
     public void OnMove(InputValue value)
@@ -97,14 +127,94 @@ public class TestShipInputEvents : MonoBehaviour
             Debug.Log("Thruster movement: " + eventValue);
 
             // Move based on thruster position.            
-            nextMovement = eventValue;
+            // nextMovement = eventValue;
+
+            //
+            // UpdateMovementSpeed(eventValue);
+            currentDepthControlValue = eventValue;
         }
         catch
         {
             Debug.Log("Thruster movement stopped");
 
             // Stop movement.
-            nextMovement = neutralSpeed;
+            // nextMovement = neutralSpeed;
+
+            //
+            // UpdateMovementSpeed(neutralSpeed);
+            currentDepthControlValue = neutralSpeed;
+        }
+    }
+
+    public void UpdateMovementSpeed(float adjustment, ref float speed, ref float topSpeed)
+    {
+        if (adjustment > 0.0f)
+        {
+            Accelerate((adjustment * accelerationRate), ref speed, ref topSpeed);
+        } else if (adjustment == 0.0f)
+        {
+            Decelerate(ref speed);
+        } else
+        {
+            Reverse((adjustment * accelerationRate * -1), ref speed, ref topSpeed);
+        }
+    }
+
+    public void Accelerate(float adjustment, ref float speed, ref float topSpeed)
+    {
+        if (speed != topSpeed)
+        {
+            if ((speed + adjustment) > topSpeed)
+            {
+                speed = topSpeed;
+            } else
+            {
+                speed += adjustment;
+            }
+        }
+    }
+
+    public void Decelerate(ref float speed)
+    {
+        if (speed != neutralSpeed)
+        {
+            if (speed > neutralSpeed)
+            {
+                if ((speed - resistance) < neutralSpeed)
+                {
+                    speed = neutralSpeed;
+                }
+                else
+                {
+                    speed -= resistance;
+                }
+            } else
+            {
+                if ((speed + resistance) > neutralSpeed)
+                {
+                    speed = neutralSpeed;
+                }
+                else
+                {
+                    speed += resistance;
+                }
+            }
+        }
+    }
+
+    public void Reverse(float adjustment, ref float speed, ref float topSpeed)
+    {
+        float topReverseMovementSpeed = (-1 * topSpeed);
+        if (speed != topReverseMovementSpeed)
+        {
+            if ((speed - adjustment) < topReverseMovementSpeed)
+            {
+                speed = topReverseMovementSpeed;
+            }
+            else
+            {
+                speed -= adjustment;
+            }
         }
     }
 
@@ -116,14 +226,20 @@ public class TestShipInputEvents : MonoBehaviour
             Debug.Log("In OnThrusterTwist: " + eventValue);
 
             // Move based on thruster position.            
-            nextHorizontalMovement = eventValue;
+            // nextHorizontalMovement = eventValue;
+
+            //
+            currentHorizontalControlValue = eventValue;
         }
         catch
         {
             Debug.Log("In OnThrusterTwist stopped");
 
             // Stop movement.
-            nextHorizontalMovement = neutralSpeed;
+            // nextHorizontalMovement = neutralSpeed;
+
+            //
+            currentHorizontalControlValue = neutralSpeed;
         }
     }
 
@@ -135,14 +251,20 @@ public class TestShipInputEvents : MonoBehaviour
             Debug.Log("In OnWeapon7: " + eventValue);
 
             // Move based on thruster position.            
-            nextVerticalMovement = eventValue;
+            // nextVerticalMovement = eventValue;
+
+            //
+            currentVerticalControlValue = eventValue;
         }
         catch
         {
             Debug.Log("In OnWeapon7 stopped");
 
             // Stop movement.
-            nextVerticalMovement = neutralSpeed;
+            // nextVerticalMovement = neutralSpeed;
+
+            //
+            currentVerticalControlValue = neutralSpeed;
         }
     }
 
@@ -154,14 +276,20 @@ public class TestShipInputEvents : MonoBehaviour
             Debug.Log("In OnWeapon8: " + eventValue);
 
             // Move based on thruster position.            
-            nextVerticalMovement = eventValue;
+            // nextVerticalMovement = eventValue;
+
+            //
+            currentVerticalControlValue = eventValue;            
         }
         catch
         {
             Debug.Log("In OnWeapon8 stopped");
 
             // Stop movement.
-            nextVerticalMovement = neutralSpeed;
+            // nextVerticalMovement = neutralSpeed;
+
+            //
+            currentVerticalControlValue = neutralSpeed;
         }
     }
 }
